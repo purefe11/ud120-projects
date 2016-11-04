26dabdc3275d0b480510f5432bbf449cf54b04c3
cheulwon <purefe11@gmail.com>
2016-11-04 17:43:48 +0900

    test

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+from sklearn.svm import SVC
+clf = SVC(kernel='rbf', C=10000.0)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print("accuracy : {}".format(accuracy))
+
+from sklearn.metrics import classification_report
+print(classification_report(labels_test, pred, target_names=['Sara', 'Chris']))
+
+from sklearn import metrics
+print(metrics.confusion_matrix(labels_test,pred))
+
+print("pred 10: {}, pred 26: {}, pred 50: {}".format(pred[10], pred[26], pred[50]))
+
+
 #########################################################
 
 
